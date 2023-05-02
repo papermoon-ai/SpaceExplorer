7680ce2ce67083589c2640c92107fe64ed4c3151
Yaroslav Donchak <eternal775@gmail.com>
2023-05-02 19:14:00 +0300

    bottom navigation

--- app/src/main/java/com/papermoon/spaceapp/features/MainActivity.kt
+++ app/src/main/java/com/papermoon/spaceapp/features/MainActivity.kt
@@ -1,31 +1,46 @@
 package com.papermoon.spaceapp.features
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.github.terrakok.cicerone.androidx.FragmentScreen
-import com.google.android.material.elevation.SurfaceColors
 import com.papermoon.spaceapp.R
 import com.papermoon.spaceapp.Screens
 import com.papermoon.spaceapp.SpaceApp
+import com.papermoon.spaceapp.databinding.ActivityMainBinding
+import com.papermoon.spaceapp.features.overview.ui.OverviewFragment
+import com.papermoon.spaceapp.features.settings.SettingsFragment
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val navigator = object: AppNavigator(this, R.id.fragment_container) {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding!!
+
+    private val router = SpaceApp.INSTANCE.router
+    private val navigationHolder = SpaceApp.INSTANCE.navigationHolder
+
+    private val navigator = object : AppNavigator(this, R.id.content_main) {
         override fun setupFragmentTransaction(
             screen: FragmentScreen,
             fragmentTransaction: FragmentTransaction,
             currentFragment: Fragment?,
             nextFragment: Fragment
         ) {
-            if (currentFragment != null) {
+            if (currentFragment == null) {
+                return
+            }
+            // Bottom navigation transition animation disable
+            else if (nextFragment is SettingsFragment || nextFragment is OverviewFragment) {
+                return
+            }
+            else {
                 fragmentTransaction.setCustomAnimations(
-                    R.anim.slide_in,
-                    R.anim.fade_out,
-                    R.anim.fade_in,
-                    R.anim.slide_out
+                    R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out
                 )
             }
         }
@@ -33,21 +48,57 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
 
-        val color = SurfaceColors.SURFACE_1.getColor(this)
-        window.statusBarColor = color
+        setContentView(binding.root)
 
-        SpaceApp.INSTANCE.router.replaceScreen(Screens.overviewScreen())
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            val currentFragment = supportFragmentManager.fragments.last()
+
+            when (item.itemId) {
+                R.id.dataOption -> {
+                    if (currentFragment !is OverviewFragment) {
+                        router.replaceScreen(Screens.overviewScreen())
+                    }
+                }
+                R.id.settingsOption -> {
+                    if (currentFragment !is SettingsFragment) {
+                        router.replaceScreen(Screens.settingsScreen())
+                    }
+                }
+                else -> return@setOnItemSelectedListener false
+            }
+
+            return@setOnItemSelectedListener true
+        }
+
+        window.statusBarColor = getColor(R.color.light_gray)
+
+        if (savedInstanceState == null) {
+            SpaceApp.INSTANCE.router.replaceScreen(Screens.overviewScreen())
+        }
+    }
+
+    fun setLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config: Configuration = resources.configuration
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 
     override fun onResume() {
         super.onResume()
-        SpaceApp.INSTANCE.navigationHolder.setNavigator(navigator)
+        navigationHolder.setNavigator(navigator)
     }
 
     override fun onPause() {
-        SpaceApp.INSTANCE.navigationHolder.removeNavigator()
+        navigationHolder.removeNavigator()
         super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/papermoon/spaceapp/features/languageSelector/ui/LanguageSelectorFragment.kt
@@ -0,0 +1,63 @@
+package com.papermoon.spaceapp.features.languageSelector.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.papermoon.spaceapp.R
+import com.papermoon.spaceapp.SpaceApp
+import com.papermoon.spaceapp.databinding.FragmentLanguageSelectorBinding
+import com.papermoon.spaceapp.features.MainActivity
+
+class LanguageSelectorFragment : Fragment() {
+    private lateinit var binding: FragmentLanguageSelectorBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLanguageSelectorBinding.inflate(inflater, container, false)
+        setupToolbar()
+
+        checkCurrentLanguageRadioButton()
+        
+        binding.radioGroupLanguages.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId) {
+                binding.btnEnglishOption.id -> {
+                    (activity as MainActivity).setLocale(getString(R.string.english_code))
+                    activity!!.recreate()
+                }
+                binding.btnRussianOption.id -> {
+                    (activity as MainActivity).setLocale(getString(R.string.russian_code))
+                    activity!!.recreate()
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun checkCurrentLanguageRadioButton() {
+        when(getString(R.string.current_language)) {
+            getString(R.string.english) -> {
+                binding.btnEnglishOption.isChecked = true
+            }
+            getString(R.string.russian) -> {
+                binding.btnRussianOption.isChecked = true
+            }
+        }
+    }
+
+    private fun setupToolbar() {
+        val toolbar = binding.toolbar.root
+        (activity as MainActivity).setSupportActionBar(toolbar)
+        (activity as MainActivity).supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        toolbar.title = getString(R.string.settings)
+        toolbar.setNavigationOnClickListener {
+            SpaceApp.INSTANCE.router.exit()
+        }
+    }
+}
